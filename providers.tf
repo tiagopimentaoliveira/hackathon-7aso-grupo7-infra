ebd50c640566a9d6342b6adc91f9ea51b25cab65
Caique Santos Amaral <caiqu.7@gmail.com>
2022-07-02 13:17:01 -0300

    Adicionando provider google-beta

--- providers.tf
+++ providers.tf
@@ -13,3 +13,9 @@ provider "google" {
   region  = "us-west1"
   zone    = "us-west1-b"
 }
+
+provider "google-beta" {
+  project = "hackathon-7aso-grupo-07"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
